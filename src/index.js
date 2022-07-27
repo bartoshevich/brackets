461149a1f0cae93213a6f09ef6fe7e2b71c2fcf6
xxx <25452644+bartoshevich@users.noreply.github.com>
2022-07-27 16:11:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+  let brackets = Object.fromEntries(bracketsConfig); 
+  let openBrackets = Object.keys(brackets);
+  // console.log(openBrackets) ;  
+  let closedBrackets = Object.values(brackets);
+  // console.log(closedBrackets);
+  
+  let revBrackets = Object.fromEntries((bracketsConfig).map(([key,value])=>[value,key]));
+  let arr = [];
+
+for (let i = 0; i < str.length; i++) {
+  if (arr.includes(str[i]) && brackets[str[i]] === revBrackets[str[i]]) {
+    arr.pop();
+  }  else if (openBrackets.includes(str[i])) {
+          arr.push(str[i]);
+} else if (closedBrackets.includes(str[i])) {
+  if (revBrackets[str[i]] !== arr.pop()) {
+        return false;
+  } 
+}
+}
+return arr.length === 0;
+ 
 }
